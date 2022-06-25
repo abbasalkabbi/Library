ca08a4501854ee08942f1f50a110cb2bf171b4db
Abbas Alkaabi <abbasalkabbi2003@gmail.com>
2022-06-26 00:52:05 +0300

    'Edit -> Add book '

--- api/Upload.php
+++ api/Upload.php
@@ -9,11 +9,13 @@ $book_name=$_FILES['pdf']['name'];
 $book_ext = pathinfo($book_name,PATHINFO_EXTENSION);// get file extension
 $tmp_book=$_FILES['pdf']['tmp_name'];// get where pdf
 if(in_array($img_ext,$img_ext_allowed) === true){
+    // if  thumbnail is  image
     if($book_ext === 'pdf'){
         $img_name_new=time().$title.'thumbnails.'.$img_ext;
         $book_name_new=time().$title.'.'.$book_ext;
         if(!empty($title)){
             if(move_uploaded_file($tmp_img,"../thumbnails/".$img_name_new) && move_uploaded_file($tmp_book,"../Books/".$book_name_new)){
+                echo json_encode(["status" => true, "message" => "تم تجميل الكتاب"]);
                 $add=mysqli_query($conn,"INSERT INTO books (title,url_img,url_pdf) VALUES('$title','$img_name_new','$book_name_new')");
             }
         }else{
@@ -23,6 +25,6 @@ if(in_array($img_ext,$img_ext_allowed) === true){
         echo json_encode(["status" => false, "message" => "Book is not Pdf"]);
     }
 }else{
-    echo json_encode(["status" => false, "message" => "Input is Empty"]);
+    echo json_encode(["status" => false, "message" => "thumbnail is not image"]);
 }
 ?>
